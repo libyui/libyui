38f54ebf00787c7780aa6c4e942f129e63065c75
Gabriele Mohr <gs@suse.de>
2003-02-06 09:18:10 +0000

    add a newline in notify popup
    svn path=/trunk/ncurses/; revision=8321

--- libyui-ncurses/src/NCPkgTable.cc
+++ libyui-ncurses/src/NCPkgTable.cc
@@ -230,7 +230,7 @@ bool NCPkgTable::changeStatus( PMSelectable::UI_Status newstatus,
 	string pkgName = objPtr->getSelectable()->name();
 	NCPopupInfo info( wpos(3, 3),
 			  header,
-			  YCPString( "<i>" + pkgName + "</i><br>" + packager->createText( notify, false ) ) );
+			  YCPString( "<i>" + pkgName + "</i><br><br>" + packager->createText( notify, false ) ) );
 	info.showInfoPopup( );
     }
     
