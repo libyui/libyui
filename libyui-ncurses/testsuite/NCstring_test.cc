79c17da88ad13d386c81a6897053702b09d6e9a0
Gabriele Mohr <gs@suse.de>
2003-07-08 13:05:52 +0000

    test program and test files for UTF-8 testing
    svn path=/trunk/ncurses/; revision=10332

---
+++ libyui-ncurses/testsuite/NCstring_test.cc
@@ -0,0 +1,316 @@
+/*---------------------------------------------------------------------\
+|                                                                      |
+|                      __   __    ____ _____ ____                      |
+|                      \ \ / /_ _/ ___|_   _|___ \                     |
+|                       \ V / _` \___ \ | |   __) |                    |
+|                        | | (_| |___) || |  / __/                     |
+|                        |_|\__,_|____/ |_| |_____|                    |
+|                                                                      |
+|                               core system                            |
+|                                                        (C) SuSE GmbH |
+\----------------------------------------------------------------------/
+
+   File:       NCstring_test.cc
+
+   Author:     Gabriele Strattner <gs@suse.de>
+   Maintainer: Michael Andres <ma@suse.de>
+
+/-*/
+#include <ycp/YCPString.h>
+
+#include "Y2Log.h"
+#include "NCstring_test.h"
+
+
+
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : NCstring_test::NCstring_test
+//	METHOD TYPE : Constructor
+//
+//	DESCRIPTION :
+//
+NCstring_test::NCstring_test( const YCPString & ystr )
+    : hotk     ( 0 )
+    , hotp     ( wstring::npos )
+{
+    bool ok = RecodeToWchar( ystr->value(), "UTF-8", &mstr );
+    if ( !ok )
+    {
+	// error logging
+    }
+}
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : NCstring_test::NCstring_test
+//	METHOD TYPE : Constructor
+//
+//	DESCRIPTION :
+//
+NCstring_test::NCstring_test( const string & str )
+    : hotk     ( 0 )
+    , hotp     ( wstring::npos )
+{
+    bool ok = RecodeToWchar( str, "UTF-8", &mstr );
+    if ( !ok )
+    {
+	// error logging
+    }
+}
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : NCstring_test::NCstring_test
+//	METHOD TYPE : Constructor
+//
+//	DESCRIPTION :
+//
+NCstring_test::NCstring_test( const char * cstr )
+    : hotk     ( 0 )
+    , hotp     ( wstring::npos )
+{
+    bool ok = RecodeToWchar( cstr, "UTF-8", &mstr );
+    if ( !ok )
+    {
+	// error logging
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : NCstring_test::operator=
+//	METHOD TYPE : NCstring_test &
+//
+//	DESCRIPTION :
+//
+NCstring_test & NCstring_test::operator=( const YCPString & ystr )
+{
+  hotk      = 0;
+  hotp      = wstring::npos;
+  bool ok =  RecodeToWchar( ystr->value(), "UTF-8", &mstr );
+  if ( !ok )
+  {
+      // error logging
+  }
+  return *this;
+}
+
+static iconv_t fromwchar_cd	= (iconv_t)(-1);
+static string  to_name		= "";
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : NCstring::RecodeFromchar
+//	METHOD TYPE : bool
+//
+//	DESCRIPTION :
+//
+bool NCstring_test::RecodeFromWchar( const wstring & in, const string & to_encoding, string* out)
+{
+    iconv_t cd = (iconv_t)(-1);
+    static bool complained = false;
+
+    // iconv_open not yet called
+    if ( fromwchar_cd == (iconv_t)(-1)
+	 || to_name != to_encoding ) 
+    {
+	if ( fromwchar_cd != (iconv_t)(-1) )
+	{
+	    iconv_close ( fromwchar_cd );
+	}
+	
+	fromwchar_cd = iconv_open ( to_encoding.c_str(), "WCHAR_T" );
+	NCMIL << "iconv_open( " << to_encoding.c_str() << ", \"WCHAR_T\" )" << endl;
+	
+	if ( fromwchar_cd == (iconv_t)(-1) )
+	{
+	    if (!complained)
+	    {
+		NCERR << "ERROR: iconv_open failed" << endl;
+		complained = true;
+	    }
+	    return false;
+	}
+	else
+	{
+	    to_name = to_encoding;
+	}
+    }
+
+    cd = fromwchar_cd;		// set iconv handle
+    
+    size_t in_len = in.length () * sizeof( wstring::value_type );	// number of in bytes
+    char* in_ptr = (char *)in.data();
+    
+    size_t tmp_size = (in_len * sizeof (char)) * 2;
+    // tmp buffer size: in_len bytes * 2, that means 1 wide charatcer (4 Byte) can be transformed
+    // into an encoding which needs at most 8 Byte for one character (should be enough)
+    
+    char tmp[tmp_size + sizeof (char)];
+
+    *out = "";
+
+    do {
+	char *tmp_ptr = tmp;
+	size_t tmp_len = tmp_size;
+	*((char*) tmp_ptr) = '\0';
+	
+	size_t iconv_ret = iconv (cd, &in_ptr, &in_len, &tmp_ptr, &tmp_len);
+
+	*((char*) tmp_ptr) = '\0';
+	*out += string ((char *)&tmp);
+
+	if (iconv_ret == (size_t)(-1))
+        {
+	    if (!complained)
+	    {
+		NCERR << "ERROR iconv: " << errno << endl;
+		complained = true;
+	    }
+	    if (errno == EINVAL || errno == EILSEQ)
+            {
+		*out += '?';
+	    }
+	    in_ptr += sizeof( wstring::value_type );
+	    in_len -= sizeof( wstring::value_type );
+	}
+	
+    } while (in_len != 0 );
+
+    return true;  
+}
+
+static iconv_t towchar_cd	= (iconv_t)(-1);
+static string  from_name 	= "";
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : NCstring::RecodeToWchar
+//	METHOD TYPE : bool
+//
+//	DESCRIPTION :
+//
+bool NCstring_test::RecodeToWchar (const string& in, const string &from_encoding, wstring* out)
+{
+    iconv_t cd = (iconv_t)(-1);
+    static bool complained = false;
+   
+    // iconv_open not yet called 
+    if ( towchar_cd == (iconv_t)(-1)
+	 || from_name != from_encoding )
+    {
+	if ( towchar_cd != (iconv_t)(-1) )
+	{
+	    iconv_close( towchar_cd );
+	}
+	
+	towchar_cd = iconv_open( "WCHAR_T", from_encoding.c_str() );
+	NCMIL << "iconv_open( \"WCHAR_T\", " << from_encoding.c_str() << " )" << endl;
+	
+	if ( towchar_cd == (iconv_t)(-1))
+	{
+	    if (!complained)
+	    {
+		NCERR << "Error: RecodeToWchar iconv_open() failed" << endl;
+		complained = true;
+	    }
+	    return false;
+	}
+	else
+	{
+	    from_name = from_encoding;
+	}
+    }
+
+    cd = towchar_cd;		// set iconv handle
+    
+    size_t in_len = in.length ();		// number of bytes of input string
+    char* in_ptr = const_cast <char*> (in.c_str ());
+    
+    size_t tmp_size = in_len * sizeof (wchar_t);	// buffer size: at most in_len wide characters
+    char tmp[tmp_size + sizeof (wchar_t)];		// + L'\0'
+
+    *out = L"";
+
+    do {
+
+	size_t tmp_len = tmp_size;
+	char* tmp_ptr = tmp;
+
+	size_t iconv_ret = iconv (cd, &in_ptr, &in_len, &tmp_ptr, &tmp_len);
+
+	*((wchar_t*) tmp_ptr) = L'\0';
+
+	*out += wstring ((wchar_t*) &tmp);
+
+	if (iconv_ret == (size_t)(-1))
+        {
+	    if ( !complained )
+	    {
+		// EILSEQ 	84	Illegal byte sequence.
+		// EINVAL   	22      Invalid argument
+		// E2BIG     	7	Argument list too long
+		NCERR << "ERROR iconv: " << errno << endl;
+		complained = true;
+	    }
+	    if (errno == EINVAL || errno == EILSEQ)
+            {
+		*out += L'?';
+	    }
+	    in_ptr++;
+	    in_len--;
+	}
+
+    } while (in_len != 0);
+
+    return true;
+}
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : NCstring_test::YCPstr
+//	METHOD TYPE : YCPString
+//
+//	DESCRIPTION :
+//
+YCPString NCstring_test::YCPstr() const
+{
+    string utf8str;
+    RecodeFromWchar ( mstr, "UTF-8", &utf8str );
+    
+    return utf8str;
+}
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : NCstring_test::getHotkey
+//	METHOD TYPE : void
+//
+//	DESCRIPTION :
+//
+void NCstring_test::getHotkey( ) const
+{
+
+    hotp = wstring::npos;
+    // strip hotkey from mstr
+    wstring::size_type tpos = mstr.find_first_of( '&' );
+    if ( tpos != wstring::npos && tpos != mstr.size()-1 )
+    {
+	mstr.erase( tpos, 1 );
+	hotk = mstr[tpos];
+	hotp = tpos;
+    }
+}
+
---
+++ libyui-ncurses/testsuite/utf8test.cc
@@ -0,0 +1,258 @@
+/*---------------------------------------------------------------------\
+|                                                                      |  
+|                      __   __    ____ _____ ____                      |  
+|                      \ \ / /_ _/ ___|_   _|___ \                     |  
+|                       \ V / _` \___ \ | |   __) |                    |  
+|                        | | (_| |___) || |  / __/                     |  
+|                        |_|\__,_|____/ |_| |_____|                    |  
+|                                                                      |  
+|                               core system                            | 
+|                                                        (C) SuSE GmbH |  
+\----------------------------------------------------------------------/ 
+
+   File:       utf8test.cc
+
+   Author:     Gabriele Strattner (gs@suse.de)
+
+/-*/
+
+#include <stdio.h>
+#include <errno.h>
+
+#include <ycp/y2log.h>
+
+#include <iostream>
+#include <string>
+
+#include <wchar.h>
+
+#include <curses.h>	/* curses.h: #define  NCURSES_CH_T cchar_t */
+
+#include "NCstring_test.h"
+
+#define typeMalloc(type,n) (type *) malloc((n) * sizeof(type))
+
+using std::string;
+using std::wstring;
+
+extern int yydebug;
+
+using namespace std;
+
+//------------------------------------------------------
+// gettext
+//------------------------------------------------------
+
+#define TEXTDOMAIN	"testdomain"
+#define LOCALEDIR	"/usr/share/locale"
+
+//------------------------------------------------------
+
+
+static void finish(int sig)
+{
+    endwin();
+    /* do your non-curses wrapup here */
+    exit(0);
+}
+
+
+//
+// main
+// 
+int main (int argc, char *argv[])
+{
+    string text = "";
+    string language = "";
+    int argp = 1;
+    bool use_utf8 = false;
+    
+    if (argc > 1)
+    {
+	while (argp < argc)
+	{
+	    if ((argv[argp][0] == '-')
+	        && (argv[argp][1] == 'l')
+	        && (argp+1 < argc))
+	    {
+		argp++;
+		y2setLogfileName (argv[argp]);
+	    }
+	    else if (text == "")
+	    {
+		text = argv[argp];
+	    }
+	    else if ( language == "" )
+	    {
+		language = argv[argp];
+	    }
+	    else
+	    {
+		fprintf (stderr, "Bad argument '%s'\nUsage: utf8test text_to_translate language\n",
+			 argv[argp]);
+		exit(1);
+	    }
+	    argp++;
+	}
+    }
+    else
+    {
+	fprintf( stderr, "Usage: utf8test text_to_translate language\n" );
+	exit(1);
+    }
+
+    //
+    // WFM parts
+    //
+
+    if ( setlocale(LC_ALL, "") == NULL )
+    {
+	fprintf(stderr,"warning: locale not supported by libc\n");
+    }
+
+    bindtextdomain( TEXTDOMAIN, LOCALEDIR);
+
+    // codeset of the terminal (so long as not yet changed by a start script or
+    // a setenv( LANG ) call)
+    char *codeset;
+
+    codeset = nl_langinfo( CODESET );
+    cout << "Initial codeset: " << codeset << endl;
+    
+    // Explicitly set LC_CTYPE to this codeset because the terminal runs with this codeset
+    // and must not be changed by setting LANG variable.
+    // export LANG= ... or setenv( LANG ) changes all LC_...variables accordingly except those
+    // explicitly set before.
+    // Functions which are respecting the locale e.g. wcwidth() are looking for the LC_CTYPE.
+
+    // setenv ( "LC_CTYPE", codeset, 1 );
+    setlocale( LC_CTYPE, codeset );
+    
+    if ( strcmp( codeset, "UTF-8") == 0 )
+    {
+	use_utf8 = true;
+    }
+
+    // always use UTF-8 for gettext() !!!
+    bind_textdomain_codeset ( TEXTDOMAIN, "UTF-8" );
+
+    textdomain ( TEXTDOMAIN );
+
+    //
+    // TEST: LC_CTYPE is not changed by the setenv () if explicitly set
+    //
+    setenv ( "LANG", "de_DE.UTF-8", 1 );
+    cout << "LANG is changed to: " <<  getenv( "LANG" ) << " - but codeset remains: "
+	 << nl_langinfo( CODESET ) << endl;
+
+    // - BUT another setlocale( LC_ALL, langugae ) call would change all LC_... variables including the  LC_CTYPE! 
+
+    //
+    // TEST: RecodeToWchar() / RecodeFromWchar()
+    //
+    bool ok;
+    wstring wtext;
+
+    // TEST cases:
+    // (all testfiles are UTF-8 encoded)
+    // - utf8test `cat testfile.cs` in cs_CZ.UTF-8 locale -> OK
+    // - utf8test öööäää            in de_DE@euro  locale  -> RecodeToWchar Not OK (input not UTF-8)
+    // - utf8test `cat testfile.cs` in de_DE@euro locale -> RecodeFromWchar Not OK (terminal cannot show czech)
+    ok = NCstring_test::RecodeToWchar( text, "UTF-8", &wtext );
+    fprintf( stderr, "RecodeToWchar(): %ls\n", wtext.c_str());
+
+    if ( ok )
+    {
+	if ( use_utf8 )
+	{
+	    fprintf( stderr, "UTF-8 terminal -> wstring not recoded: %ls\n", wtext.c_str());
+	}
+	else
+	{
+	    string recoded;
+	    bool ok = NCstring_test::RecodeFromWchar( wtext, codeset, &recoded );
+	    if (ok )
+	    {
+		fprintf( stderr, "%s terminal -> RecodeFromWchar(): %s\n", codeset, recoded.c_str());
+	    }
+	    else
+	    {
+		fprintf( stderr, "wstring NOT recoded" );
+	    }
+	}
+    }
+    else
+    {
+	fprintf( stderr, "Cannot create wstring\n");
+    }
+
+    //--------------------------------------------------------------------------- 
+    // Curses INIT
+    //---------------------------------------------------------------------------
+
+    (void) initscr();      // initialize the curses library
+    keypad(stdscr, TRUE);  // enable keyboard mapping 
+    (void) nonl();         // tell curses not to do NL->CR/NL on output 
+    (void) cbreak();       // take input chars one at a time, no wait for \n 
+    
+    (void) noecho();	   // echo OFF (don't echo input)
+    nodelay(stdscr, FALSE);   // delay mode  FALSE: get_wch waits for an input
+                              //             TRUE: no celay
+    if (has_colors())
+    {
+        start_color();
+	/* Simple color assignment, often all we need.  Color pair 0 cannot
+	 * be redefined. */
+	init_pair(1, COLOR_RED,     COLOR_BLACK);
+        init_pair(2, COLOR_GREEN,   COLOR_BLACK);
+        init_pair(3, COLOR_YELLOW,  COLOR_BLACK);
+    }
+
+    //-----------------------------------------------------------------------------
+    
+    int line = 1;
+    size_t i;
+    
+    NCstring_test textinput;
+    
+    textinput = text;
+    
+    for (;;)
+    {
+	wint_t k;
+
+	// waiting for input
+        get_wch( &k );
+
+	line = line+2;
+	mvprintw( line, 1, "The argument (converted in wstring): \n" );
+	
+	for ( i = 0; i <= textinput.str().length() ; i++ )
+	{
+	    wchar_t out[CCHARW_MAX+1];
+	    out[0] =  textinput.str()[i];
+	    out[1] = L'\0';
+	    addwstr( out );	
+	}
+	
+	size_t outwidth = 0;
+	for ( i = 0; i <= textinput.str().length() ; i++ )
+	{
+	    // The behaviour of wcwidth depends on the LC_CTYPE  category
+	    // of the current locale.
+	    // That means width is correct for UTF-8 text on UTF-8 terminal
+	    // and UTF-8 text with luit on terminal with encoding corresponding
+	    // to the language of input and for text of encoding which
+	    // corresponds encoding of the terminal
+	    outwidth += wcwidth( textinput.str()[i] );
+	}
+	line = line+2;
+	mvprintw( line, 1, "Width on the terminal %d:\n", outwidth );
+
+	refresh();
+
+    }
+
+
+    finish(0);               /* we're done */	
+}
