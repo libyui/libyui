a4b1522f360a3bbbecd8a4594e758856641a2b38
Gabriele Mohr <gs@suse.de>
2003-09-05 10:27:31 +0000

    remove unused code
    svn path=/trunk/ncurses/; revision=11753

--- libyui-ncurses/src/ncursesw.cc
+++ libyui-ncurses/src/ncursesw.cc
@@ -570,13 +570,6 @@ int NCursesWindow::box( const wrect & dim )
     addch( area.Pos.L, area.Pos.C+area.Sze.W-1, ACS_URCORNER );
     addch( area.Pos.L+area.Sze.H-1, area.Pos.C+area.Sze.W-1, ACS_LRCORNER );
     addch( area.Pos.L,              area.Pos.C, ACS_ULCORNER );
-// FIXME ???
-#if 0
-    mvadd_wch( area.Pos.L+area.Sze.H-1, area.Pos.C, WACS_LLCORNER );
-    mvadd_wch( area.Pos.L, area.Pos.C+area.Sze.W-1, WACS_URCORNER );
-    mvadd_wch( area.Pos.L+area.Sze.H-1, area.Pos.C+area.Sze.W-1, WACS_LRCORNER );
-    mvadd_wch( area.Pos.L,              area.Pos.C, WACS_ULCORNER );
-#endif
   }
 
   return OK;
