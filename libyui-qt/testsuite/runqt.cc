ad02530591f87c45bf844f575415df765cadfa8d
Michal Švec <msvec@suse.cz>
2003-12-12 20:29:11 +0000

    - adapted for the logging changes in liby2util
    svn path=/trunk/qt/; revision=13033

--- libyui-qt/testsuite/runqt.cc
+++ libyui-qt/testsuite/runqt.cc
@@ -40,7 +40,7 @@ main (int argc, char *argv[])
 	        && (argv[argp][1] == 'l')
 	        && (argp+1 < argc)) {
 		argp++;
-		y2setLogfileName (argv[argp]);
+		set_log_filename (argv[argp]);
 	    }
 	    else if (fname == 0) {
 		fname = argv[argp];
