b352ba14f4616d732c447d1796c2aaae6ba990f5
Klaus Kämpf <kkaempf@suse.de>
2008-05-16 16:03:26 +0000

    works now (Thanks, jw!)
    svn path=/trunk/libyui-bindings/; revision=47637

--- libyui-bindings/swig/perl/examples/hello_world.pl
+++ libyui-bindings/swig/perl/examples/hello_world.pl
@@ -5,9 +5,11 @@
 
 use yui;
 
-my $dialog = yui::YUI->widgetFactory()->create_popup_dialog();
-#my $vbox = ->create_vbox( $dialog );
-#factory->create_label( $vbox, "Hello, World!" );
-#factory->create_push_button( $vbox, "&OK" );
-#my $event = $dialog->wait_for_event();
+my $factory = yui::YUI::widgetFactory;
+my $dialog = $factory->createPopupDialog;
+
+my $vbox = $factory->createVBox( $dialog );
+$factory->createLabel( $vbox, "Hello, World!" );
+$factory->createPushButton( $vbox, "&OK" );
+my $event = $dialog->waitForEvent();
 #$dialog->destroy();
