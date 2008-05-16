7c73c26d66d5e5ad40e658e53ddf78bf9d6b0394
Klaus Kämpf <kkaempf@suse.de>
2008-05-16 17:18:58 +0000

    works now !
    svn path=/trunk/libyui-bindings/; revision=47638

--- libyui-bindings/swig/python/examples/hello_world.py
+++ libyui-bindings/swig/python/examples/hello_world.py
@@ -6,9 +6,10 @@ import sys
 sys.path.append('../../../build/bindings/python')
 import yui
 
-dialog = yui.YWidgetFactory.createPopupDialog()
-vbox = yui.YWidgetFactory_createVBox( dialog )
-yui.YWidgetFactory_createLabel( vbox, "Hello, World!" )
-yui.YWidgetFactory_createPushButton( vbox, "&OK" )
+factory = yui.YUI.widgetFactory();
+dialog = factory.createPopupDialog();
+vbox = factory.createVBox( dialog )
+factory.createLabel( vbox, "Hello, World!" )
+factory.createPushButton( vbox, "&OK" )
 event = dialog.waitForEvent()
 dialog.destroy()
