ea8d77efd3ad524abae15406c2b4d556b064bff8
Arvin Schnell <aschnell@suse.de>
2009-01-21 17:10:06 +0000

    - take filename and layout from command-line
    svn path=/trunk/qt-graph/; revision=54867

--- libyui-qt-graph/examples/SimpleLoad.cc
+++ libyui-qt-graph/examples/SimpleLoad.cc
@@ -4,6 +4,8 @@
 // Trivial example loading a graph from a file.
 
 
+#include <iostream>
+
 #include "YUI.h"
 #include "YWidgetFactory.h"
 #include "YOptionalWidgetFactory.h"
@@ -16,10 +18,16 @@
 int
 main(int argc, char** argv)
 {
+    if (argc != 3)
+    {
+	std::cerr << argv[0] << " filename layout" << std::endl;
+	return 1;
+    }
+
     YDialog* dialog = YUI::widgetFactory()->createPopupDialog();
     YLayoutBox* vbox = YUI::widgetFactory()->createVBox(dialog);
 
-    YUI::optionalWidgetFactory()->createGraph(vbox, "simple.dot", "dot");
+    YUI::optionalWidgetFactory()->createGraph(vbox, argv[1], argv[2]);
 
     dialog->waitForEvent();
     dialog->destroy();
