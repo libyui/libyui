f59cf56482b651d09fdfb8b22082dfc2c22624ab
Michael Andres <ma@suse.de>
2009-05-27 13:16:02 +0000

    Remove use of deprecated Package::filenames.
    svn path=/trunk/ncurses-pkg/; revision=57339

--- libyui-ncurses-pkg/src/NCPkgPackageDetails.cc
+++ libyui-ncurses-pkg/src/NCPkgPackageDetails.cc
@@ -175,7 +175,7 @@ void NCPkgPackageDetails::technicalData( ZyppObj pkgPtr, ZyppSel slbPtr )
 	text += "-";
 	text += package->sourcePkgEdition().asString();
 	text += "<br>";
-	
+
 	//authors, in one line
         text += NCPkgStrings::Authors();
         list<string> authors = package->authors(); // zypp::Package
@@ -198,7 +198,8 @@ void NCPkgPackageDetails::fileList( ZyppSel slbPtr )
        text += commonHeader( slbPtr->theObj() );
        text += NCPkgStrings::ListOfFiles();
        // get the file list from the package manager/show the list
-       list<string> fileList = package->filenames();
+       zypp::Package::FileList pkgfilelist( package->filelist() );
+       list<string> fileList( pkgfilelist.begin(), pkgfilelist.end() );
        text += createText( fileList, false ) ;
    }
 
