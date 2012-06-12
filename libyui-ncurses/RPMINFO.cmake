44925c44d45ead4620123e9f8881eb2672f0650c
Björn Esser <bjoern.esser@gmail.com>
2012-06-12 11:04:52 +0200

    setup rpm-spec-info

--- libyui-ncurses/RPMINFO.cmake
+++ libyui-ncurses/RPMINFO.cmake
@@ -2,20 +2,10 @@
 #####
 ##### THIS IS THE INFO REQUIRED FOR SPEC-FILE GENERATION
 
-SET( SPEC_URL			"http://github.com/libyui/" )							# the URL of the project
-
-SET( SPEC_SUMMARY		"" )										# some brief summary
-
-SET( SPEC_DESCRIPTION		"\n" )										# the description to be used, end each line with "\n" for line-breaks
-
-SET( SPEC_BuildRequires		"cmake >= 2.8" )								# the BuildRequires every single pkg wrapped with "" and speparated with spaces
-
-SET( SPEC_Conflicts		"" )										# the Conflicts every single pkg wrapped with "" and speparated with spaces
-
-SET( SPEC_Provides		"" )										# the Provides every single pkg wrapped with "" and speparated with spaces
-
-SET( SPEC_Obsoletes		"" )										# the Obsoletes every single pkg wrapped with "" and speparated with spaces
-
-SET( SPEC_DEVEL_Requires	"@PROJECTNAME@@@varpfx@_SONAME_MAJOR@ = %{version}" )				# the Requires for the -devel pkg every single pkg wrapped with "" and speparated with spaces
-
-SET( SPEC_DEVEL_Provides	"pkgconfig(@PROJECTNAME@) = %{version}" )					# the Provides for the -devel pkg
+SET( SPEC_URL			"http://github.com/libyui/" )
+SET( SPEC_SUMMARY		"libYUI - Character Based User Interface" )
+SET( SPEC_DESCRIPTION		"This package contains the character based (ncurses) user interface\ncomponent for libYUI.\n" )
+SET( SPEC_BuildRequires		"cmake >= 2.8" "gcc-c++" "libtool" "libyui-devel" "ncurses-devel" )
+SET( SPEC_Obsoletes		"yast2-ncurses < %{version}" )
+SET( SPEC_DEVEL_Requires	"@PROJECTNAME@@@varpfx@_SONAME_MAJOR@ = %{version}" "libyui-devel" "ncurses-devel" )
+SET( SPEC_DEVEL_Provides	"pkgconfig(@PROJECTNAME@) = %{version}" )
