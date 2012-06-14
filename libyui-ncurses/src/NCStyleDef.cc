8a17ba420e93cfe2d4bf859a4258a242c8163d21
Björn Esser <bjoern.esser@gmail.com>
2012-06-14 14:28:55 +0200

    update copyright-headers to LGPL 2.1/3.0

--- libyui-ncurses/src/NCStyleDef.cc
+++ libyui-ncurses/src/NCStyleDef.cc
@@ -1,25 +1,16 @@
 /*
-  |****************************************************************************
-  |
-  | Copyright (c) 2000 - 2012 Novell, Inc.
-  | All Rights Reserved.
-  |
-  | This program is free software; you can redistribute it and/or
-  | modify it under the terms of version 2 of the GNU General Public License as
-  | published by the Free Software Foundation.
-  |
-  | This program is distributed in the hope that it will be useful,
-  | but WITHOUT ANY WARRANTY; without even the implied warranty of
-  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
-  | GNU General Public License for more details.
-  |
-  | You should have received a copy of the GNU General Public License
-  | along with this program; if not, contact Novell, Inc.
-  |
-  | To contact Novell about this file by physical or electronic mail,
-  | you may find current contact information at www.novell.com
-  |
-  |****************************************************************************
+  Copyright (C) 1970-2012 Novell, Inc
+  This library is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 2.1 of the
+  License, or (at your option) version 3.0 of the License. This library
+  is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+  License for more details. You should have received a copy of the GNU
+  Lesser General Public License along with this library; if not, write
+  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+  Floor, Boston, MA 02110-1301 USA
 */
 
 
@@ -1453,27 +1444,18 @@ void NCStyleDef::saveStyle()
     p.refresh();
 
     out << "/*" << std::endl;
-    out << "  |****************************************************************************" << std::endl;
-    out << "  |" << std::endl;
-    out << "  | Copyright (c) 2000 - 2012 Novell, Inc." << std::endl;
-    out << "  | All Rights Reserved." << std::endl;
-    out << "  |" << std::endl;
-    out << "  | This program is free software; you can redistribute it and/or" << std::endl;
-    out << "  | modify it under the terms of version 2 of the GNU General Public License as" << std::endl;
-    out << "  | published by the Free Software Foundation." << std::endl;
-    out << "  |" << std::endl;
-    out << "  | This program is distributed in the hope that it will be useful," << std::endl;
-    out << "  | but WITHOUT ANY WARRANTY; without even the implied warranty of" << std::endl;
-    out << "  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the" << std::endl;
-    out << "  | GNU General Public License for more details." << std::endl;
-    out << "  |" << std::endl;
-    out << "  | You should have received a copy of the GNU General Public License" << std::endl;
-    out << "  | along with this program; if not, contact Novell, Inc." << std::endl;
-    out << "  |" << std::endl;
-    out << "  | To contact Novell about this file by physical or electronic mail," << std::endl;
-    out << "  | you may find current contact information at www.novell.com" << std::endl;
-    out << "  |" << std::endl;
-    out << "  |****************************************************************************" << std::endl;
+    out << "  Copyright (C) 1970-2012 Novell, Inc" << std::endl;
+    out << "  This library is free software; you can redistribute it and/or modify" << std::endl;
+    out << "  it under the terms of the GNU Lesser General Public License as" << std::endl;
+    out << "  published by the Free Software Foundation; either version 2.1 of the" << std::endl;
+    out << "  License, or (at your option) version 3.0 of the License. This library" << std::endl;
+    out << "  is distributed in the hope that it will be useful, but WITHOUT ANY" << std::endl;
+    out << "  WARRANTY; without even the implied warranty of MERCHANTABILITY or " << std::endl;
+    out << "  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public" << std::endl;
+    out << "  License for more details. You should have received a copy of the GNU" << std::endl;
+    out << "  Lesser General Public License along with this library; if not, write" << std::endl;
+    out << "  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth" << std::endl;
+    out << "  Floor, Boston, MA 02110-1301 USA" << std::endl;
     out << "*/" << std::endl;
     out << std::endl;
     out << std::endl;
