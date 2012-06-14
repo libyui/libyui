e9b109e53f5dddea78e8477bc4981a6c8c9d3f55
Björn Esser <bjoern.esser@gmail.com>
2012-06-14 16:44:55 +0200

    capitalize Summary

--- libyui-ncurses/RPMINFO.cmake
+++ libyui-ncurses/RPMINFO.cmake
@@ -3,7 +3,7 @@
 ##### THIS IS THE INFO REQUIRED FOR SPEC-FILE GENERATION
 
 SET( SPEC_Url			"http://github.com/libyui/" )
-SET( SPEC_Summary		"libYUI - Character Based User Interface" )
+SET( SPEC_Summary		"Libyui - Character Based User Interface" )
 SET( SPEC_Description		"This package contains the character based (ncurses) user interface\ncomponent for libYUI.\n" )
 SET( SPEC_BuildRequires		"cmake >= 2.8" "gcc-c++" "libtool" "libyui-devel" "ncurses-devel" )
 SET( SPEC_Obsoletes		"yast2-ncurses < %{version}" )
