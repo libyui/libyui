5977e2345faeee1d45ba1bfd2a10fd6b36625d91
Michael Andres <ma@suse.de>
2012-06-14 13:33:45 +0200

    update from buildtools

--- libyui-qt/RPMINFO.cmake
+++ libyui-qt/RPMINFO.cmake
@@ -2,11 +2,11 @@
 #####
 ##### THIS IS THE INFO REQUIRED FOR SPEC-FILE GENERATION
 
-SET( SPEC_URL			"http://github.com/libyui/libyui-qt/" )						# the URL of the project
+SET( SPEC_Url			"http://github.com/libyui/libyui-qt/" )						# the URL of the project
 
-SET( SPEC_SUMMARY		"Qt frontend for libyui" )							# some brief summary
+SET( SPEC_Summary		"Qt frontend for libyui" )							# some brief summary
 
-SET( SPEC_DESCRIPTION		"This is the user interface frontend that provides Qt UI for libyui\n" )	# the description to be used, end each line with "\n" for line-breaks
+SET( SPEC_Description		"This is the user interface frontend that provides Qt UI for libyui\n" )	# the description to be used, end each line with "\n" for line-breaks
 
 SET( SPEC_BuildRequires		"cmake >= 2.8" "libyui-devel >= 2.21.5" "gcc-c++" "libqt4-devel" "fontconfig-devel" )			# the BuildRequires every single pkg wrapped with "" and speparated with spaces
 
