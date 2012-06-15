a66221ab3838b5051d6890c4dc85a01061d4e7d0
Michael Andres <ma@suse.de>
2012-06-15 12:06:47 +0200

    provide/obsolete yast2-qt

--- libyui-qt/RPMINFO.cmake
+++ libyui-qt/RPMINFO.cmake
@@ -8,15 +8,15 @@ SET( SPEC_Summary		"Qt frontend for libyui" )							# some brief summary
 
 SET( SPEC_Description		"This is the user interface frontend that provides Qt UI for libyui\n" )	# the description to be used, end each line with "\n" for line-breaks
 
-SET( SPEC_BuildRequires		"cmake >= 2.8" "libyui-devel >= 2.21.5" "gcc-c++" "libqt4-devel" "fontconfig-devel" )			# the BuildRequires every single pkg wrapped with "" and speparated with spaces
+SET( SPEC_BuildRequires		"cmake >= 2.8" "libyui-devel >= 2.21.5" "gcc-c++" "libqt4-devel" "fontconfig-devel" )	# the BuildRequires every single pkg wrapped with "" and speparated with spaces
 
 SET( SPEC_Conflicts		"" )										# the Conflicts every single pkg wrapped with "" and speparated with spaces
 
-SET( SPEC_Provides		"py2qt = %{version}" )                                                          # the Provides every single pkg wrapped with "" and speparated with spaces
+SET( SPEC_Provides		"py2qt = %{version}" "yast2-qt = 2.22.6" )					# the Provides every single pkg wrapped with "" and speparated with spaces
 
-SET( SPEC_Obsoletes		"py2qt < %{version}" )							        # the Obsoletes every single pkg wrapped with "" and speparated with spaces
+SET( SPEC_Obsoletes		"py2qt < %{version}" "yast2-qt < 2.22.6" )				        # the Obsoletes every single pkg wrapped with "" and speparated with spaces
 
-SET( SPEC_Docs			"%doc COPYING.GPL2" )										# for docs that should be in the main-pkg
+SET( SPEC_Docs			"%doc COPYING.GPL2" )								# for docs that should be in the main-pkg
 
 SET( SPEC_DEVEL_Requires	"@PROJECTNAME@@@varpfx@SONAME_MAJOR@ = %{version}" )				# the Requires for the -devel pkg every single pkg wrapped with "" and speparated with spaces
 SET( SPEC_DEVEL_Requires	${SPEC_DEVEL_Requires} "glibc-devel" "libstdc++-devel" )
