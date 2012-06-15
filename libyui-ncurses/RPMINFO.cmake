5dc690e85d475d083d0204dcacc7718253a2aedb
Michael Andres <ma@suse.de>
2012-06-15 09:11:32 +0200

    update from buildtools

--- libyui-ncurses/RPMINFO.cmake
+++ libyui-ncurses/RPMINFO.cmake
@@ -8,5 +8,5 @@ SET( SPEC_Description		"This package contains the character based (ncurses) user
 SET( SPEC_BuildRequires		"cmake >= 2.8" "gcc-c++" "libtool" "libyui-devel" "ncurses-devel" )
 SET( SPEC_Obsoletes		"yast2-ncurses < %{version}" )
 SET( SPEC_Docs                  "%doc %dir %{_docdir}/@PROJECTNAME@@SONAME_MAJOR@\n%doc %{_docdir}/@PROJECTNAME@@SONAME_MAJOR@/COPYING*" )
-SET( SPEC_DEVEL_Requires	"@PROJECTNAME@@@varpfx@_SONAME_MAJOR@ = %{version}" "libyui-devel" "ncurses-devel" )
+SET( SPEC_DEVEL_Requires	"@PROJECTNAME@@@varpfx@SONAME_MAJOR@ = %{version}" "libyui-devel" "ncurses-devel" )
 SET( SPEC_DEVEL_Provides	"pkgconfig(@PROJECTNAME@) = %{version}" )
