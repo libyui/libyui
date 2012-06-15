f0d8aa258f1206eb495d661f230a917495c73b74
Michael Andres <ma@suse.de>
2012-06-15 13:26:44 +0200

    update from buildtools

--- libyui-qt/RPMINFO.cmake
+++ libyui-qt/RPMINFO.cmake
@@ -10,11 +10,13 @@ SET( SPEC_Description		"This is the user interface frontend that provides Qt UI 
 
 SET( SPEC_BuildRequires		"cmake >= 2.8" "libyui-devel >= 2.21.5" "gcc-c++" "libqt4-devel" "fontconfig-devel" )	# the BuildRequires every single pkg wrapped with "" and speparated with spaces
 
-SET( SPEC_Conflicts		"" )										# the Conflicts every single pkg wrapped with "" and speparated with spaces
+SET( SPEC_Requires		"" )										# the Requires every single pkg wrapped with "" and speparated with spaces
 
 SET( SPEC_Provides		"py2qt = %{version}" "yast2-qt = 2.22.6" )					# the Provides every single pkg wrapped with "" and speparated with spaces
 
 SET( SPEC_Obsoletes		"py2qt < %{version}" "yast2-qt < 2.22.6" )				        # the Obsoletes every single pkg wrapped with "" and speparated with spaces
+
+SET( SPEC_Conflicts		"" )										# the Conflicts every single pkg wrapped with "" and speparated with spaces
 
 SET( SPEC_Docs			"%doc COPYING.GPL2" )								# for docs that should be in the main-pkg
 
