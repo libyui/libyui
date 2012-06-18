8d60304214f432a4eaf229bb48e09b78073ae356
Michael Andres <ma@suse.de>
2012-06-18 10:24:03 +0200

    fix specfile

--- libyui-qt-pkg/RPMINFO.cmake
+++ libyui-qt-pkg/RPMINFO.cmake
@@ -10,6 +10,7 @@ SET( SPEC_Description
   "This is the yast2 package selector widget plugin provided for the libyui Qt UI\n" )		# the description to be used, end each line with "\n" for line-breaks
 
 SET( SPEC_BuildRequires		"cmake >= 2.8"
+  "libyui-devel"
   "libyui-qt-devel >= 2.21.6"
   "libzypp-devel >= 11.4.0"
 )								# the BuildRequires every single pkg wrapped with "" and speparated with spaces
@@ -24,7 +25,9 @@ SET( SPEC_Conflicts		"" )										# the Conflicts every single pkg wrapped with
 
 SET( SPEC_Docs			"" )				# for docs that should be in the main-pkg
 
-SET( SPEC_DEVEL_Requires	"@PROJECTNAME@@@varpfx@SONAME_MAJOR@ = %{version}" )	# the Requires for the -devel pkg every single pkg wrapped with "" and speparated with spaces
+SET( SPEC_DEVEL_Requires	"@PROJECTNAME@@@varpfx@SONAME_MAJOR@ = %{version}"
+  "libyui-qt-devel >= 2.21.6"
+)	# the Requires for the -devel pkg every single pkg wrapped with "" and speparated with spaces
 
 SET( SPEC_DEVEL_Provides	"pkgconfig(@PROJECTNAME@) = %{version}"
   "yast2_ui_pkg"
