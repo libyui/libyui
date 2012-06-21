8f3774b5b291d2f70476a0a2c1e51e02ce22f2b1
Michael Andres <ma@suse.de>
2012-06-21 13:30:15 +0200

    we want to link against ncursesw.6

--- libyui-ncurses/PROJECTINFO.cmake
+++ libyui-ncurses/PROJECTINFO.cmake
@@ -6,9 +6,18 @@ SET( BASELIB		"yui" )		# don't change this
 
 SET( SUBDIRS		src )
 SET( PLUGINNAME		"ncurses" )
-SET( LIB_DEPS		Curses )
+SET( LIB_DEPS		)
 SET( INTERNAL_DEPS	Libyui )
-SET( LIB_LINKER		ncurses ncursesw panelw )
+SET( LIB_LINKER		)
 SET( URL		"http://github.com/libyui/" )
 SET( SUMMARY		"Libyui - Character Based User Interface" )
 SET( DESCRIPTION	"This package contains the character based (ncurses) user interface\ncomponent for libYUI.\n" )
+
+# using ncurses6:
+SET ( NCURSES_CFLAGS	"-I/usr/include/ncurses6" )
+SET ( NCURSES_LIBS	"-L/usr/lib64/ncurses6 -lncursesw -lpanelw" )
+
+SET ( CMAKE_CXX_FLAGS	"${NCURSES_CFLAGS} ${CMAKE_CXX_FLAGS}" )
+SET ( CMAKE_C_FLAGS	"${NCURSES_CFLAGS} ${CMAKE_C_FLAGS}" )
+SET ( CMAKE_SHARED_LINKER_FLAGS	"${NCURSES_LIBS} ${CMAKE_SHARED_LINKER_FLAGS}" )
+ADD_DEFINITIONS( "-DNCURSES_INTERNALS=1" )
