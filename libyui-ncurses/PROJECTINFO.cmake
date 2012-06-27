a2ee55a6e0a5f7a27eb90cc56c51b2b3e090f708
Michael Andres <ma@suse.de>
2012-06-27 12:52:19 +0200

    add FindCurses6.cmake to buildtools

--- libyui-ncurses/PROJECTINFO.cmake
+++ libyui-ncurses/PROJECTINFO.cmake
@@ -12,3 +12,7 @@ SET( LIB_LINKER		)
 SET( URL		"http://github.com/libyui/" )
 SET( SUMMARY		"Libyui - Character Based User Interface" )
 SET( DESCRIPTION	"This package contains the character based (ncurses) user interface\ncomponent for libYUI.\n" )
+
+SET( BUILDTOOLS_LIST
+  cmake/Modules/FindCurses6.cmake
+)
