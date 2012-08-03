fa277c34e8ae3601a7ac31765fa614e8ea807efa
Thomas Goettlicher <tgoettlicher@suse.de>
2012-08-03 14:54:51 +0200

    fixed license entries

--- libyui-ncurses/src/NCStyleDef.cc
+++ libyui-ncurses/src/NCStyleDef.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 1970-2012 Novell, Inc
+  Copyright (C) 2000-2012 Novell, Inc
   This library is free software; you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as
   published by the Free Software Foundation; either version 2.1 of the
@@ -1440,7 +1440,7 @@ void NCStyleDef::saveStyle()
     p.refresh();
 
     out << "/*" << std::endl;
-    out << "  Copyright (C) 1970-2012 Novell, Inc" << std::endl;
+    out << "  Copyright (C) 2000-2012 Novell, Inc" << std::endl;
     out << "  This library is free software; you can redistribute it and/or modify" << std::endl;
     out << "  it under the terms of the GNU Lesser General Public License as" << std::endl;
     out << "  published by the Free Software Foundation; either version 2.1 of the" << std::endl;
