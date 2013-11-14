b227cdbe23d038dccaa7c07f076073d87e93f485
Martin Vidner <mvidner@suse.cz>
2013-11-14 17:26:49 +0100

    today's experiments; i want to drop buildtools and code the makefiles from scratch

--- libyui-qt/PROJECTINFO.cmake
+++ libyui-qt/PROJECTINFO.cmake
@@ -6,13 +6,13 @@ SET( BASELIB		"yui" )		# don't change this
 
 SET( SUBDIRS		src )	        # set the subdirs where your code is located.
 SET( PLUGINNAME		"qt" )		# define the your plugin's name, e.g. {gtk, ncurses-pkg, qt-graph}
-SET( LIB_DEPS		Qt5Widgets)		# define the plugin's dependecies CMake should look for separated with spaces; libyui-internal deps go in the next line
+SET( LIB_DEPS		Qt5Core Qt5Gui Qt5Widgets Qt5Svg Qt5X11Extras)		# define the plugin's dependecies CMake should look for separated with spaces; libyui-internal deps go in the next line
 # Find includes in corresponding build directories
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 # Instruct CMake to run moc automatically when needed.
 set(CMAKE_AUTOMOC ON)
 SET( INTERNAL_DEPS	Libyui )	# include libyui or it's plugins (if needed), too!
-SET( LIB_LINKER		Qt5Core Qt5Gui Qt5Widgets Qt5Svg Qt5X11Extras X11 fontconfig dl pthread )	# define the libs to link against with their -l name for separated with spaces, e.g. pthread dl; include libyui or it's plugins (if needed), too!
+SET( LIB_LINKER		X11 fontconfig dl pthread )	# define the libs to link against with their -l name for separated with spaces, e.g. pthread dl; include libyui or it's plugins (if needed), too!
 SET( EXTRA_INCLUDES	/usr/include/qt5 /usr/include/qt5/QtCore /usr/include/qt5/QtGui /usr/include/qt5/QtWidgets /usr/include/qt5/QtSvg /usr/include/qt5/QtX11Extras)		# set include-dir which are not picked by CMake automagically here.
 SET( PROGSUBDIR		"" )		# define your programm's name if the plugin is made for it, only. e.g. "YaST2" for {gtk,ncurses,qt}-pkg.
 SET( URL                "http://github.com/libyui/" )
