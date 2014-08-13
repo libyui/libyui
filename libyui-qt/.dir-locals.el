1783a8c572448bf03f6e9e89a124c06469a7082f
Martin Vidner <mvidner@suse.cz>
2014-08-13 17:04:19 +0200

    repo-wide indentation settings for Emacs

---
+++ libyui-qt/.dir-locals.el
@@ -0,0 +1,3 @@
+; https://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html#Directory-Variables
+((c++-mode . ((indent-tabs-mode . nil)
+              (c-basic-offset   . 4) )))
