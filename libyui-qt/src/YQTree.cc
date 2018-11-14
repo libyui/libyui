48349236511a1e8f3fc56109eeb0ab6701c9e166
hellcp <hellcp@opensuse.org>
2018-11-14 16:24:04 +0100

    Fix the rest of issues with icon

--- libyui-qt/src/YQTree.cc
+++ libyui-qt/src/YQTree.cc
@@ -493,9 +493,7 @@ void YQTreeItem::init( YQTree *		tree,
     if ( _origItem->hasIconName() )
     {
         // This icon loader is different from the rest, because YaST Partitioner breaks otherwise
-        QIcon icon = QIcon( _tree->iconFullPath( _origItem ).c_str() );
-        if (QIcon::hasThemeIcon( _origItem->iconName().c_str() ) )
-            icon = YQUI::ui()->loadIcon( _origItem->iconName() );
+        QIcon icon = YQUI::ui()->loadIcon( _origItem->iconName() );
 
 	if ( !icon.isNull() )
 	    setData( 0, Qt::DecorationRole, icon );
