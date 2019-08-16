mvidner/libyui-combined-alpha-1

Bump version and update changes file

libyui-qt/VERSION.cmake
@@ -1,6 +1,6 @@
 SET(VERSION_MAJOR "2")
 SET(VERSION_MINOR "49")
-SET(VERSION_PATCH "1")
+SET(VERSION_PATCH "2")
 SET( VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}${GIT_SHA1_VERSION}" )
 
 ##### This is needed for the libyui-qt core ONLY.

libyui/libyui

libyui-qt/VERSION.cmake
@@ -1,7 +1,7 @@
 SET(VERSION_MAJOR "2")
 SET(VERSION_MINOR "47")
 SET(VERSION_PATCH "1")
-SET(VERSION_TWEAK "1")
+SET(VERSION_TWEAK "2")
 SET( VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}.${VERSION_TWEAK}${GIT_SHA1_VERSION}" )
 
 ##### This is need for the libyui core, ONLY.