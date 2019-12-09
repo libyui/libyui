97af12e2404491246496b3ec7b8cde5d26c2a4f7
Ladislav Slezák <lslezak@suse.cz>
2019-12-09 11:39:29 +0100

    Revert "Remove the groups view (fate#326485) (libyui/libyui-qt-pkg#75)"
    This reverts commit 9347d0c677f915e25a3b40e750fcf01f163b3508.

--- libyui-qt-pkg/SOURCECONF.cmake
+++ libyui-qt-pkg/SOURCECONF.cmake
@@ -26,6 +26,7 @@ SET( ${TARGETLIB}_SOURCES
   YQPkgLangList.cc
   YQPkgList.cc
   YQPkgObjList.cc
+  YQPkgPackageKitGroupsFilterView.cc
   YQPkgPatchFilterView.cc
   YQPkgPatchList.cc
   YQPkgPatternList.cc
@@ -74,6 +75,7 @@ SET( ${TARGETLIB}_HEADERS
   YQPkgLangList.h
   YQPkgList.h
   YQPkgObjList.h
+  YQPkgPackageKitGroupsFilterView.h
   YQPkgPatchFilterView.h
   YQPkgPatchList.h
   YQPkgPatternList.h
