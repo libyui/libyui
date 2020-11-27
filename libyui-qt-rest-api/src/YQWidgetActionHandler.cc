4a15ffd154f0f68afb110b5448193cd37fc107ab
Rodion Iafarov <riafarov@suse.com>
2020-11-27 11:37:00 +0100

    Split widget actions handlers to separate classes
    Not to keep all the code, creating structure to implement handling
    of each widget action in separate class. So far it only creates one
    base class and separate handler for YTable, which needs overrides for
    activation in qt. Base implementation will be used for ncurses.

---
+++ libyui-qt-rest-api/src/YQWidgetActionHandler.cc
@@ -0,0 +1,58 @@
+/*
+  Copyright (C) 2020 SUSE LLC
+
+  This library is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 2.1 of the
+  License, or (at your option) version 3.0 of the License. This library
+  is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+  License for more details. You should have received a copy of the GNU
+  Lesser General Public License along with this library; if not, write
+  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+  Floor, Boston, MA 02110-1301 USA
+*/
+
+#include "YQWidgetActionHandler.h"
+
+void YQWidgetActionHandler::activate_widget( YCheckBoxFrame * widget )
+{
+    widget->handleChildrenEnablement( widget->value() );
+    activate_qt_widget( widget );
+}
+
+void YQWidgetActionHandler::activate_widget( YComboBox * widget )
+{
+    activate_qt_widget( widget );
+}
+
+void YQWidgetActionHandler::activate_widget( YDateField * widget )
+{
+    activate_qt_widget( widget );
+}
+
+void YQWidgetActionHandler::activate_widget( YInputField * widget )
+{
+    activate_qt_widget( widget );
+}
+
+void YQWidgetActionHandler::activate_widget( YRadioButton * widget )
+{
+    activate_qt_widget( widget );
+}
+
+void YQWidgetActionHandler::activate_widget( YSelectionBox * widget )
+{
+    activate_qt_widget( widget );
+}
+
+void YQWidgetActionHandler::activate_widget( YTimeField * widget )
+{
+    activate_qt_widget( widget );
+}
+
+void YQWidgetActionHandler::activate_widget ( YMultiSelectionBox * widget, YItem * item )
+{
+    activate_qt_widget( widget, item );
+}
