ef2d0511005c9930f29baa9499534809a3e3cfe9
Stefan Hundhammer <shundhammer@suse.de>
2021-01-13 20:24:57 +0100

    Killed utterly wrong and misleading comment

--- libyui-qt/VERSION.cmake
+++ libyui-qt/VERSION.cmake
@@ -3,8 +3,6 @@ SET(VERSION_MINOR "56")
 SET(VERSION_PATCH "4")
 SET( VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}${GIT_SHA1_VERSION}" )
 
-##### This is needed for the libyui-qt core ONLY.
-##### These will be overridden from exports in LibyuiConfig.cmake
 SET( SONAME_MAJOR "14" )
 SET( SONAME_MINOR "0" )
 SET( SONAME_PATCH "0" )
